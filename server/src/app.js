fee9040a59b63ece43498a4de1f3c6592927550b
joseluisvegas <161845982+joseluisvegas@users.noreply.github.com>
2026-04-08 18:37:44 -0500

    corrigiendo los cors

--- server/src/app.js
+++ server/src/app.js
@@ -8,6 +8,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors({
   origin: "*",
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
 app.use((req, res, next) => {
